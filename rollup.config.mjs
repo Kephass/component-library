b62d04e7b18252d8a12426c8dcb10fe1c775bb09
Kephass <odurof99@gmail.com>
2023-03-27 22:30:42 +0200

    fix rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 
-import packageJson from './package.json';
+import packageJson from './package.json' assert { type: 'json' };
 
 export default [
     {
@@ -28,7 +28,7 @@ export default [
     },
     {
         input: 'dist/esm/types/index.d.ts',
-        output: [{ file: 'dist/index.d.ts', format: 'es' }],
+        output: [{ file: 'dist/index.d.ts', format: 'esm' }],
         plugins: [dts()],
     }
 ]
